a441c6e1287c48a823a22e2bca3f3e2ee4c84bb6
Yuganshu Kumar <69767140+YTheLynch@users.noreply.github.com>
2025-04-18 11:25:29 +0530

    Delete 0189-rotate-array/0189-rotate-array.cpp

--- 0189-rotate-array/0189-rotate-array.cpp
+++
@@ -1,7 +0,0 @@
-class Solution {
-public:
-    void rotate(vector<int>& nums, int k) {
-        k = k % nums.size(); 
-        ::rotate(nums.begin(), nums.begin() + nums.size() - k, nums.end());
-    }
-};
